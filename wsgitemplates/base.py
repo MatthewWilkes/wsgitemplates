211751ed71d4b6698ba703c6c54dfcf2fa68930a
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2009-07-05 21:06:21 +0100

    Move the custom run to the base template

--- wsgitemplates/base.py
+++ wsgitemplates/base.py
@@ -6,7 +6,10 @@ class BaseTemplate(templates.Template):
     use_cheetah = True
     
     def run(self, command, output_dir, vars):
-        templates.Template.run(self, command, output_dir, vars)
+        output_dir = os.path.join(*([vars['egg'], 'src'] + vars['segs']))
+        self.pre(command, output_dir, vars)
+        self.write_files(command, output_dir, vars)
+        self.post(command, output_dir, vars)
 
     def post(self, *args, **kargs):
         templates.Template.post(self, *args, **kargs)
--- wsgitemplates/middleware_template.py
+++ wsgitemplates/middleware_template.py
@@ -13,9 +13,3 @@ class Middleware(BaseTemplate):
                               ' (lowercase letters only, no special chars)'),
         ]
 
-    
-    def run(self, command, output_dir, vars):
-        output_dir = os.path.join(*([vars['egg'], 'src'] + vars['segs']))
-        self.pre(command, output_dir, vars)
-        self.write_files(command, output_dir, vars)
-        self.post(command, output_dir, vars)
